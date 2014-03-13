f1f8c5142d9c775750f844338cd23275bb3ab7e8
parroty <parroty00@gmail.com>
2014-03-13 23:20:16 +0900

    Add package.exs

---
+++ package.exs
@@ -0,0 +1,4 @@
+Expm.Package.new(name: "extwitter", description: "Twitter client library for elixir",
+                 version: "0.0.1", keywords: ["Elixir","Twitter","REST"],
+                 maintainers: [[name: "parroty", email: "parroty00@gmail.com"]],
+                 repositories: [[github: "parroty/extwitter"]])
