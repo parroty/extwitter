1a891a0ccc3ae5c2c78a39843a5de8191a646c83
parroty <parroty00@gmail.com>
2014-08-06 17:03:05 +0900

    Update deps

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule ExTwitter.Mixfile do
     [
       {:oauth, github: "tim/erlang-oauth"},
       {:jsex, "~> 2.0"},
-      {:exvcr, "~> 0.2", only: [:dev, :test]},
+      {:exvcr, "~> 0.3", only: [:dev, :test]},
       {:excoveralls, "~> 0.3", only: :dev},
       {:meck, "0.8.2", github: "eproxus/meck", tag: "0.8.2", only: :test},
       {:mock, github: "jjh42/mock", only: :test}
