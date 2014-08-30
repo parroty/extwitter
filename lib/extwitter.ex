7e1dc1736e142d076a4115987769eb1f97084305
parroty <parroty00@gmail.com>
2014-08-31 01:07:28 +0900

    List up unimplemented APIs, as a kind of TODO list

--- lib/extwitter.ex
+++ lib/extwitter.ex
@@ -5,6 +5,8 @@ defmodule ExTwitter do
 
   use Application
 
+  # -------------- Timelines -------------
+
   @doc """
   GET statuses/mentions_timeline
   https://dev.twitter.com/docs/api/1.1/get/statuses/mentions_timeline
@@ -32,6 +34,8 @@ defmodule ExTwitter do
   """
   defdelegate retweets_of_me,          to: ExTwitter.API.Timelines
   defdelegate retweets_of_me(options), to: ExTwitter.API.Timelines
+
+  # -------------- Tweets -------------
 
   @doc """
   GET statuses/retweets/:id
@@ -61,6 +65,15 @@ defmodule ExTwitter do
   defdelegate update(status),          to: ExTwitter.API.Tweets
   defdelegate update(status, options), to: ExTwitter.API.Tweets
 
+  # POST statuses/retweet/:id
+  # https://dev.twitter.com/docs/api/1.1/post/statuses/retweet/%3Aid
+
+  # POST statuses/update_with_media
+  # https://dev.twitter.com/docs/api/1.1/post/statuses/update_with_media
+
+  # GET statuses/oembed
+  # https://dev.twitter.com/docs/api/1.1/get/statuses/oembed
+
   @doc """
   GET statuses/retweeters/ids
   https://dev.twitter.com/docs/api/1.1/get/statuses/retweeters/ids
@@ -68,12 +81,16 @@ defmodule ExTwitter do
   defdelegate retweeter_ids(id),          to: ExTwitter.API.Tweets
   defdelegate retweeter_ids(id, options), to: ExTwitter.API.Tweets
 
+  # -------------- Search -------------
+
   @doc """
   GET search/tweets
   https://dev.twitter.com/docs/api/1.1/get/search/tweets
   """
   defdelegate search(query),          to: ExTwitter.API.Search
   defdelegate search(query, options), to: ExTwitter.API.Search
+
+  # -------------- Streaming -------------
 
   @doc """
   GET statuses/sample
@@ -99,26 +116,36 @@ defmodule ExTwitter do
   defdelegate stream_control(pid, :stop),          to: ExTwitter.API.Streaming
   defdelegate stream_control(pid, :stop, options), to: ExTwitter.API.Streaming
 
-  @doc """
-  GET geo/search
-  https://dev.twitter.com/docs/api/1.1/get/geo/search
-  """
-  defdelegate geo_search(query),          to: ExTwitter.API.PlacesAndGeo
-  defdelegate geo_search(query, options), to: ExTwitter.API.PlacesAndGeo
+  # --------------  Direct Messages -------------
 
-  @doc """
-  GET geo/reverse_geocode
-  https://dev.twitter.com/docs/api/1.1/get/geo/reverse_geocode
-  """
-  defdelegate reverse_geocode(lat, long),          to: ExTwitter.API.PlacesAndGeo
-  defdelegate reverse_geocode(lat, long, options), to: ExTwitter.API.PlacesAndGeo
+  # -------------- Friends & Followers -------------
 
-  @doc """
-  GET trends/place
-  https://api.twitter.com/1.1/trends/place.json
-  """
-  defdelegate trends(id),          to: ExTwitter.API.Trends
-  defdelegate trends(id, options), to: ExTwitter.API.Trends
+  # GET friendships/no_retweets/ids
+  # https://dev.twitter.com/docs/api/1.1/get/friendships/no_retweets/ids
+
+  # GET friends/ids
+  # https://dev.twitter.com/docs/api/1.1/get/friends/ids
+
+  # GET followers/ids
+  # https://dev.twitter.com/docs/api/1.1/get/followers/ids
+
+  # GET friendships/incoming
+  # https://dev.twitter.com/docs/api/1.1/get/friendships/incoming
+
+  # GET friendships/outgoing
+  # https://dev.twitter.com/docs/api/1.1/get/friendships/outgoing
+
+  # POST friendships/create
+  # https://dev.twitter.com/docs/api/1.1/post/friendships/create
+
+  # POST friendships/destroy
+  # https://dev.twitter.com/docs/api/1.1/post/friendships/destroy
+
+  # POST friendships/update
+  # https://dev.twitter.com/docs/api/1.1/post/friendships/update
+
+  # GET friendships/show
+  # https://dev.twitter.com/docs/api/1.1/get/friendships/show
 
   @doc """
   GET followers/list
@@ -134,6 +161,49 @@ defmodule ExTwitter do
   defdelegate friends(screen_name),          to: ExTwitter.API.FriendsAndFollowers
   defdelegate friends(screen_name, options), to: ExTwitter.API.FriendsAndFollowers
 
+  # GET friendships/lookup
+  # https://dev.twitter.com/docs/api/1.1/get/friendships/lookup
+
+  # -------------- Users -------------
+
+  # GET account/settings
+  # https://dev.twitter.com/docs/api/1.1/get/account/settings
+
+  # GET account/verify_credentials
+  # https://dev.twitter.com/docs/api/1.1/get/account/verify_credentials
+
+  # POST account/settings
+  # https://dev.twitter.com/docs/api/1.1/post/account/settings
+
+  # POST account/update_delivery_device
+  # https://dev.twitter.com/docs/api/1.1/post/account/update_delivery_device
+
+  # POST account/update_profile
+  # https://dev.twitter.com/docs/api/1.1/post/account/update_profile
+
+  # POST account/update_profile_background_image
+  # https://dev.twitter.com/docs/api/1.1/post/account/update_profile_background_image
+
+  # POST account/update_profile_colors
+  # https://dev.twitter.com/docs/api/1.1/post/account/update_profile_colors
+
+  # POST account/update_profile_image
+  # https://dev.twitter.com/docs/api/1.1/post/account/update_profile_image
+
+  # GET blocks/list
+  # https://dev.twitter.com/docs/api/1.1/get/blocks/list
+
+  # GET blocks/ids
+  # https://dev.twitter.com/docs/api/1.1/get/blocks/ids
+
+  # POST blocks/create
+  # https://dev.twitter.com/docs/api/1.1/post/blocks/create
+
+  # POST blocks/destroy
+  # https://dev.twitter.com/docs/api/1.1/post/blocks/destroy
+
+  # GET users/lookup
+  # https://dev.twitter.com/docs/api/1.1/get/users/lookup
 
   @doc """
   GET users/show
@@ -149,12 +219,57 @@ defmodule ExTwitter do
   defdelegate user_search(query),          to: ExTwitter.API.Users
   defdelegate user_search(query, options), to: ExTwitter.API.Users
 
+  # GET users/contributees
+  # https://dev.twitter.com/docs/api/1.1/get/users/contributees
+
+  # GET users/contributors
+  # https://dev.twitter.com/docs/api/1.1/get/users/contributors
+
+  # POST account/remove_profile_banner
+  # https://dev.twitter.com/docs/api/1.1/post/account/remove_profile_banner
+
+  # POST account/update_profile_banner
+  # https://dev.twitter.com/docs/api/1.1/post/account/update_profile_banner
+
+  # POST mutes/users/create
+  # https://dev.twitter.com/docs/api/1.1/post/mutes/users/create
+
+  # POST mutes/users/destroy
+  # https://dev.twitter.com/docs/api/1.1/post/mutes/users/destroy
+
+  # GET mutes/users/ids
+  # https://dev.twitter.com/docs/api/1.1/get/mutes/users/ids
+
+  # GET mutes/users/list
+  # https://dev.twitter.com/docs/api/1.1/get/mutes/users/list
+
+  # -------------- Suggested Users -------------
+
+  # GET users/suggestions/:slug
+  # https://dev.twitter.com/docs/api/1.1/get/users/suggestions/%3Aslug
+
+  # GET users/suggestions
+  # https://dev.twitter.com/docs/api/1.1/get/users/suggestions
+
+  # GET users/suggestions/:slug/members
+  # https://dev.twitter.com/docs/api/1.1/get/users/suggestions/%3Aslug/members
+
+  # -------------- Favorites -------------
+
   @doc """
   GET favorites/list
   https://dev.twitter.com/docs/api/1.1/get/favorites/list
   """
   defdelegate favorites,          to: ExTwitter.API.Favorites
   defdelegate favorites(options), to: ExTwitter.API.Favorites
+
+  # POST favorites/destroy
+  # https://dev.twitter.com/docs/api/1.1/post/favorites/destroy
+
+  # POST favorites/create
+  # https://dev.twitter.com/docs/api/1.1/post/favorites/create
+
+  # -------------- Lists -------------
 
   @doc """
   GET lists/list
@@ -171,6 +286,9 @@ defmodule ExTwitter do
   defdelegate list_timeline(list, owner),          to: ExTwitter.API.Lists
   defdelegate list_timeline(list, owner, options), to: ExTwitter.API.Lists
 
+  # POST lists/members/destroy
+  # https://dev.twitter.com/docs/api/1.1/post/lists/members/destroy
+
   @doc """
   GET lists/memberships
   https://dev.twitter.com/docs/api/1.1/get/lists/memberships
@@ -185,6 +303,100 @@ defmodule ExTwitter do
   defdelegate list_subscribers(options),              to: ExTwitter.API.Lists
   defdelegate list_subscribers(list, owner),          to: ExTwitter.API.Lists
   defdelegate list_subscribers(list, owner, options), to: ExTwitter.API.Lists
+
+  # POST lists/subscribers/create
+  # https://dev.twitter.com/docs/api/1.1/post/lists/subscribers/create
+
+  # GET lists/subscribers/show
+  # https://dev.twitter.com/docs/api/1.1/get/lists/subscribers/show
+
+  # POST lists/subscribers/destroy
+  # https://dev.twitter.com/docs/api/1.1/post/lists/subscribers/destroy
+
+  # POST lists/members/create_all
+  # https://dev.twitter.com/docs/api/1.1/post/lists/members/create_all
+
+  # GET lists/members/show
+  # https://dev.twitter.com/docs/api/1.1/get/lists/members/show
+
+  # GET lists/members
+  # https://dev.twitter.com/docs/api/1.1/get/lists/members
+
+  # POST lists/members/create
+  # https://dev.twitter.com/docs/api/1.1/post/lists/members/create
+
+  # POST lists/destroy
+  # https://dev.twitter.com/docs/api/1.1/post/lists/destroy
+
+  # POST lists/update
+  # https://dev.twitter.com/docs/api/1.1/post/lists/update
+
+  # POST lists/create
+  # https://dev.twitter.com/docs/api/1.1/post/lists/create
+
+  # GET lists/show
+  # https://dev.twitter.com/docs/api/1.1/get/lists/show
+
+  # GET lists/subscriptions
+  # https://dev.twitter.com/docs/api/1.1/get/lists/subscriptions
+
+  # POST lists/members/destroy_all
+  # https://dev.twitter.com/docs/api/1.1/post/lists/members/destroy_all
+
+  # GET lists/ownerships
+  # https://dev.twitter.com/docs/api/1.1/get/lists/ownerships
+
+  # -------------- Saved Searches -------------
+
+  # -------------- Places & Geo -------------
+
+  # GET geo/id/:place_id
+  # https://dev.twitter.com/docs/api/1.1/get/geo/id/%3Aplace_id
+
+  @doc """
+  GET geo/reverse_geocode
+  https://dev.twitter.com/docs/api/1.1/get/geo/reverse_geocode
+  """
+  defdelegate reverse_geocode(lat, long),          to: ExTwitter.API.PlacesAndGeo
+  defdelegate reverse_geocode(lat, long, options), to: ExTwitter.API.PlacesAndGeo
+
+  @doc """
+  GET geo/search
+  https://dev.twitter.com/docs/api/1.1/get/geo/search
+  """
+  defdelegate geo_search(query),          to: ExTwitter.API.PlacesAndGeo
+  defdelegate geo_search(query, options), to: ExTwitter.API.PlacesAndGeo
+
+  # GET geo/similar_places
+  # https://dev.twitter.com/docs/api/1.1/get/geo/similar_places
+
+  # POST geo/place
+  # https://dev.twitter.com/docs/api/1.1/post/geo/place
+
+  # -------------- Trends -------------
+
+  @doc """
+  GET trends/place
+  https://api.twitter.com/1.1/trends/place.json
+  """
+  defdelegate trends(id),          to: ExTwitter.API.Trends
+  defdelegate trends(id, options), to: ExTwitter.API.Trends
+
+  # GET trends/available
+  # https://dev.twitter.com/docs/api/1.1/get/trends/available
+
+  # GET trends/closest
+  # https://dev.twitter.com/docs/api/1.1/get/trends/closest
+
+  # -------------- Spam Reporting -------------
+
+  # -------------- OAuth -------------
+
+  # -------------- Help -------------
+
+  # -------------- Tweets -------------
+
+  # -------------- ExTwitter Settings -------------
 
   @doc """
   Provides OAuth configuration setting for accessing twitter server.
