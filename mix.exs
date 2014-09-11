84d5835248275f7c862b0dd05220acd6795bc36e
parroty <parroty00@gmail.com>
2014-09-12 00:21:01 +0900

    Requires elixir v1.0.0 for catching up to deprecations

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExTwitter.Mixfile do
   def project do
     [ app: :extwitter,
       version: "0.1.5",
-      elixir: "~> 0.14.3 or ~> 0.15.0 or ~> 1.0.0",
+      elixir: "~> 1.0.0",
       deps: deps,
       description: description,
       package: package,
