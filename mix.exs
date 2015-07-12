8a3b669ca36ec1ceaaa1a609ae37cb3b36980663
parroty <parroty00@gmail.com>
2015-07-12 12:20:22 +0900

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExTwitter.Mixfile do
 
   def project do
     [ app: :extwitter,
-      version: "0.4.0",
+      version: "0.4.1",
       elixir: "~> 1.0.0",
       deps: deps,
       description: description,
