07731534465cfdb280abe3e4ac4470346e82682c
parroty <parroty00@gmail.com>
2015-10-21 22:00:59 +0900

    Fix application naming in config.exs (#25, #26)

---
+++ config/config.exs
@@ -0,0 +1,8 @@
+use Mix.Config
+
+config :extwitter, :oauth, [
+   consumer_key: System.get_env("TWITTER_CONSUMER_KEY"),
+   consumer_secret: System.get_env("TWITTER_CONSUMER_SECRET"),
+   access_token: System.get_env("TWITTER_ACCESS_TOKEN"),
+   access_token_secret: System.get_env("TWITTER_ACCESS_SECRET")
+]
--- lib/extwitter/config.ex
+++ lib/extwitter/config.ex
@@ -7,14 +7,17 @@ defmodule ExTwitter.Config do
   Get OAuth configuration values.
   """
   def get, do: get(current_scope)
-  def get(:global), do: Application.get_env(:ex_twitter, :oauth, nil)
+  def get(:global) do
+    Application.get_env(:extwitter, :oauth, nil) ||
+    Application.get_env(:ex_twitter, :oauth, nil)
+  end
   def get(:process), do: Process.get(:_ex_twitter_oauth, nil)
 
   @doc """
   Set OAuth configuration values.
   """
   def set(value), do: set(current_scope, value)
-  def set(:global, value), do: Application.put_env(:ex_twitter, :oauth, value)
+  def set(:global, value), do: Application.put_env(:extwitter, :oauth, value)
   def set(:process, value) do
     Process.put(:_ex_twitter_oauth, value)
     :ok
