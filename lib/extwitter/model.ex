476c0b32e6726737beb4c1f25a3ef02309efb8e0
parroty <parroty00@gmail.com>
2015-10-29 23:47:56 +0900

    Add missing attributes for Tweet, User, Entities models (#28)

--- lib/extwitter/model.ex
+++ lib/extwitter/model.ex
@@ -5,14 +5,18 @@ defmodule ExTwitter.Model.Tweet do
   ## Reference
   https://dev.twitter.com/overview/api/tweets
   """
-  defstruct contributors: nil, coordinates: nil, created_at: nil, entities: nil,
-    favorite_count: nil, favorited: nil, geo: nil, id: nil, id_str: nil,
+  defstruct contributors: nil, coordinates: nil, created_at: nil,
+    current_user_retweet: nil, entities: nil, favorite_count: nil,
+    favorited: nil, filter_level: nil, geo: nil, id: nil, id_str: nil,
     in_reply_to_screen_name: nil, in_reply_to_status_id: nil,
     in_reply_to_status_id_str: nil, in_reply_to_user_id: nil,
     in_reply_to_user_id_str: nil, lang: nil, place: nil,
-    retweet_count: nil, retweeted: nil, source: nil, text: nil,
-    truncated: nil, user: nil
-
+    possibly_sensitive: nil, quoted_status_id: nil,
+    quoted_status_id_str: nil, quoted_status: nil, scopes: nil,
+    retweet_count: nil, retweeted: nil, retweeted_status: nil,
+    source: nil, text: nil, truncated: nil, user: nil,
+    withheld_copyright: nil, withheld_in_countries: nil,
+    withheld_scope: nil
   @type t :: %__MODULE__{}
 end
 
@@ -29,21 +33,23 @@ defmodule ExTwitter.Model.User do
   ## Reference
   https://dev.twitter.com/overview/api/users
   """
-  defstruct contributors_enabled: nil, created_at: nil, default_profile: nil,
-    default_profile_image: nil, description: nil, entities: nil,
-    favourites_count: nil, follow_request_sent: nil, followers_count: nil,
-    following: nil, friends_count: nil, geo_enabled: nil, id: nil,
-    id_str: nil, is_translation_enabled: nil, is_translator: nil,
-    lang: nil, listed_count: nil, location: nil, name: nil,
-    notifications: nil, profile_background_color: nil,
+  defstruct contributors_enabled: nil, created_at: nil,
+    default_profile: nil, default_profile_image: nil, description: nil,
+    entities: nil, favourites_count: nil, follow_request_sent: nil,
+    followers_count: nil, following: nil, friends_count: nil,
+    geo_enabled: nil, id: nil, id_str: nil, is_translation_enabled: nil,
+    is_translator: nil, lang: nil, listed_count: nil, location: nil,
+    name: nil, notifications: nil, profile_background_color: nil,
     profile_background_image_url: nil,
     profile_background_image_url_https: nil, profile_background_tile: nil,
-    profile_image_url: nil, profile_image_url_https: nil,
-    profile_link_color: nil, profile_sidebar_border_color: nil,
-    profile_sidebar_fill_color: nil, profile_text_color: nil,
-    profile_use_background_image: nil, protected: nil, screen_name: nil,
-    statuses_count: nil, time_zone: nil, url: nil, utc_offset: nil,
-    verified: nil
+    profile_banner_url: nil, profile_image_url: nil,
+    profile_image_url_https: nil, profile_link_color: nil,
+    profile_sidebar_border_color: nil, profile_sidebar_fill_color: nil,
+    profile_text_color: nil, profile_use_background_image: nil,
+    protected: nil, screen_name: nil, show_all_inline_media: nil,
+    status: nil, statuses_count: nil, time_zone: nil, url: nil,
+    utc_offset: nil, verified: nil, withheld_in_countries: nil,
+    withheld_scope: nil
 
   @type t :: %__MODULE__{}
 end
@@ -55,7 +61,8 @@ defmodule ExTwitter.Model.Entities do
   ## Reference
   https://dev.twitter.com/overview/api/entities
   """
-  defstruct hashtags: nil, symbols: nil, urls: nil, user_mentions: nil
+  defstruct hashtags: nil, media: nil, symbols: nil, urls: nil,
+    user_mentions: nil
 
   @type t :: %__MODULE__{}
 end
