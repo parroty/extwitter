b1c4ab68f5bd4e6691055f8009740869bcd6a272
parroty <parroty00@gmail.com>
2015-12-20 20:47:11 +0900

    Update dependencies

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,7 @@
     - Replaces ExTwitter.request_token/0 with ExTwitter.request_token/1 which has redirect_url parameter.
     - Removes redirect_url paramter from ExTwitter.authorize_url.
     - Removes redirect_url paramter from ExTwitter.authenticate_url.
+* Update versions of dependent modules.
 
 0.5.5
 ------
--- mix.exs
+++ mix.exs
@@ -30,15 +30,15 @@ defmodule ExTwitter.Mixfile do
   def deps do
     [
       {:oauth, github: "tim/erlang-oauth"},
-      {:poison, "~> 1.3"},
-      {:timex, "~> 0.13"},
-      {:exvcr, "~> 0.5", only: :test},
+      {:poison, "~> 1.5"},
+      {:timex, "~> 0.19"},
+      {:exvcr, "~> 0.6", only: :test},
       {:excoveralls, "~> 0.4", only: :test},
       {:meck, "~> 0.8.2", only: [:dev, :test]},
       {:mock, github: "parroty/mock", only: [:dev, :test], branch: "fix"},
-      {:ex_doc, "~> 0.10", only: :docs},
+      {:ex_doc, "~> 0.11", only: :docs},
       {:earmark, "~> 0.1", only: :docs},
-      {:inch_ex, "~> 0.2", only: :docs},
+      {:inch_ex, "~> 0.4", only: :docs},
       {:benchfella, github: "alco/benchfella", only: :dev}
     ]
   end
