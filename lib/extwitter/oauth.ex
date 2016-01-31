b7710ee42d5dae398b74c24589a5872239848202
parroty <parroty00@gmail.com>
2016-01-31 10:59:23 +0900

    Add fallback logic for original erlang-oauth use

--- lib/extwitter/oauth.ex
+++ lib/extwitter/oauth.ex
@@ -7,28 +7,44 @@ defmodule ExTwitter.OAuth do
   Send request with get method.
   """
   def request(:get, url, params, consumer, access_token, access_token_secret) do
-    :oauth.get(url, params, consumer, access_token, access_token_secret, [], proxy_option)
+    if has_funtion(:get, 7) do
+      :oauth.get(url, params, consumer, access_token, access_token_secret, [], proxy_option)
+    else
+      :oauth.get(url, params, consumer, access_token, access_token_secret, [])
+    end
   end
 
   @doc """
   Send request with post method.
   """
   def request(:post, url, params, consumer, access_token, access_token_secret) do
-    :oauth.post(url, params, consumer, access_token, access_token_secret, [], proxy_option)
+    if has_funtion(:post, 7) do
+      :oauth.post(url, params, consumer, access_token, access_token_secret, [], proxy_option)
+    else
+      :oauth.post(url, params, consumer, access_token, access_token_secret, [])
+    end
   end
 
   @doc """
   Send async request with get method.
   """
   def request_async(:get, url, params, consumer, access_token, access_token_secret) do
-    :oauth.get(url, params, consumer, access_token, access_token_secret, stream_option, proxy_option)
+    if has_funtion(:get, 7) do
+      :oauth.get(url, params, consumer, access_token, access_token_secret, stream_option, proxy_option)
+    else
+      :oauth.get(url, params, consumer, access_token, access_token_secret, stream_option)
+    end
   end
 
   @doc """
   Send async request with post method.
   """
   def request_async(:post, url, params, consumer, access_token, access_token_secret) do
-    :oauth.post(url, params, consumer, access_token, access_token_secret, stream_option, proxy_option)
+    if has_funtion(:post, 7) do
+      :oauth.post(url, params, consumer, access_token, access_token_secret, stream_option, proxy_option)
+    else
+      :oauth.post(url, params, consumer, access_token, access_token_secret, stream_option)
+    end
   end
 
   defp stream_option do
@@ -37,5 +53,10 @@ defmodule ExTwitter.OAuth do
 
   defp proxy_option do
     ExTwitter.Proxy.options
+  end
+
+  defp has_funtion(function, arity) do
+    :oauth.module_info[:exports]
+    |> Enum.find(fn({k, v}) -> k == function && v == arity end)
   end
 end
